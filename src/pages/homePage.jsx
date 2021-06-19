e76416506de62e864645a76ff9d041ea3326a397
Ade Mahendra <hendraadem@gmail.com>
2021-06-19 22:56:44 +0700

    add home page pick products

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 import HeaderCarousel from "../component/home/headerCarousel";
+import axios from "axios";
+import HomeProductList from "../component/product/homeProductList";
 
 class HomePage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      homeProducts: [],
+    };
+  }
+  componentDidMount = () => {
+    axios.get("http://localhost:2000/products").then((res) => {
+      this.setState({ homeProducts: res.data });
+    });
+  };
+  showHomeProduct = () => {};
   render() {
     return (
       <div>
@@ -10,7 +24,15 @@ class HomePage extends React.Component {
             <HeaderCarousel></HeaderCarousel>
           </div>
         </div>
-        <div className="container"></div>
+        <div className="container p-0">
+          <div className="product-list" style={style.homeProductListHeader}>
+            <h5>For you</h5>
+            <a href="">see more</a>
+          </div>
+          <div>
+            <HomeProductList />
+          </div>
+        </div>
       </div>
     );
   }
@@ -22,6 +44,11 @@ const style = {
     height: "400px",
     backgroundColor: "#fff",
     paddingTop: "70px",
+  },
+  homeProductListHeader: {
+    display: "flex",
+    justifyContent: "space-between",
+    marginTop: "20px",
   },
 };
 
