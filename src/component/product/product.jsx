215ac78b1493dbf39520fb85b2b2bce66762686c
Ade Mahendra <hendraadem@gmail.com>
2021-06-20 22:45:48 +0700

    fetch data from props

--- src/component/product/product.jsx
+++ src/component/product/product.jsx
@@ -1,44 +1,61 @@
 import React from "react";
 import { Card, Button, Badge, Tooltip } from "react-bootstrap";
-import product from "../../assets/product/mbp.jpg";
+import { Link } from "react-router-dom";
 
 class Product extends React.Component {
   render() {
     return (
-      <Card style={style.productCard}>
-        <div style={style.cardTopAction}>
-          <Badge variant="danger" style={style.discountBadge}>
-            10% Off
-          </Badge>
-        </div>
-        <div style={style.cardImageWrapper}>
-          <Card.Img style={style.cardImage} variant="top" src={product} />
-        </div>
-        <Button variant="default" style={style.addToWishlistBtn}>
-          <i class="far fa-heart"></i>
-        </Button>
-        <Card.Body className="p-2 pb-2">
-          <p style={style.productTitle}>Macbook pro 2021</p>
-          <p style={style.productPrice}>Rp 21.000.000</p>
-          <div style={style.productQuickDetail}>
-            <Badge variant="light" style={style.badgeRating}>
-              <i class="fas fa-star text-warning"></i> 3.4
-            </Badge>
-            <Badge
-              variant="light"
-              className="text-success"
-              style={style.badgeOrders}
-            >
-              <i class="fas fa-shopping-bag text-success"></i> 49 orders
-            </Badge>
-            <Badge
-              variant="light"
-              className="text-default"
-              style={style.badgeReviews}
-            >
-              31 reviews
+      <div style={style.productCardWrapper}>
+        <Card style={style.productCard}>
+          <div style={style.cardTopAction}>
+            <Badge variant="danger" style={style.discountBadge}>
+              10% Off
             </Badge>
           </div>
+          <div style={style.cardImageWrapper}>
+            <Card.Img
+              style={style.cardImage}
+              variant="top"
+              src={this.props.productImage}
+            />
+          </div>
+          <Button variant="default" style={style.addToWishlistBtn}>
+            <i class="far fa-heart"></i>
+          </Button>
+          <Card.Body
+            className="p-2 pb-2"
+            style={style.cardBody}
+            as={Link}
+            to="/"
+          >
+            <div style={style.productTitleWrapper} as="{Link}" to="/">
+              <p style={style.productTitle}>
+                {this.props.productTitle.toLowerCase()}
+              </p>
+            </div>
+            <p style={style.productPrice}>
+              Rp {this.props.productPrice.toLocaleString()}
+            </p>
+            <div style={style.productQuickDetail}>
+              <Badge variant="light" style={style.badgeRating}>
+                <i class="fas fa-star text-warning"></i> 3.4
+              </Badge>
+              <Badge
+                variant="light"
+                className="text-success"
+                style={style.badgeOrders}
+              >
+                <i class="fas fa-shopping-bag text-success"></i> 49 orders
+              </Badge>
+              <Badge
+                variant="light"
+                className="text-default"
+                style={style.badgeReviews}
+              >
+                31 reviews
+              </Badge>
+            </div>
+          </Card.Body>
           <div style={style.productCardFooter}>
             <Button
               variant="primary"
@@ -49,19 +66,26 @@ class Product extends React.Component {
               add to card <i className="fas fa-cart-plus ml-1"></i>
             </Button>
           </div>
-        </Card.Body>
-      </Card>
+        </Card>
+      </div>
     );
   }
 }
 
 const style = {
+  productCardWrapper: {
+    display: "inline-block",
+  },
   productCard: {
     border: "none",
-    width: "250px",
+    width: "215px",
+    marginRight: "10px",
     boxSizing: "border-box",
     marginBottom: "100px",
     boxShadow: "0 4px 2px rgba(0, 0, 0, 0.01), 0 2px 2px 0 rgba(0, 0, 0, 0.06)",
+  },
+  cardBody: {
+    textDecoration: "none",
   },
   cardTopAction: {
     position: "absolute",
@@ -88,11 +112,17 @@ const style = {
     height: "100%",
     objectFit: "cover",
   },
+  productTitleWrapper: {
+    height: "40px",
+    width: "150px",
+  },
   productTitle: {
-    fontSize: "16px",
+    fontSize: "14px",
     marginBottom: "0",
     fontWeight: "500",
     color: "#3A4047",
+    textDecoration: "none",
+    lineHeight: "1.4",
   },
   productQuickDetail: {
     fontSize: "14px",
@@ -107,17 +137,18 @@ const style = {
     fontWeight: "normal",
     backgroundColor: "#fff",
   },
-
   productPrice: {
     fontSize: "12px",
     fontWeight: "600",
     color: "#F36E2D",
     marginBottom: "0px",
+    marginTop: "3px",
   },
+
+  productCardFooter: { margin: "10px" },
   addToCartBtn: {
     fontSize: "12px",
     fontWeight: "600",
-    marginTop: "20px",
   },
   addToWishlistBtn: {
     padding: "6px 8px",
